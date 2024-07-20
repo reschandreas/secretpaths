1e667534d998b6353119cab1b0fee4ccd55a80df
Andreas Resch <andreas@resch.io>
2024-07-20 11:39:44 +0200

    refactor

--- visualizer/src/routes/dynamic/+page.svelte
+++ visualizer/src/routes/dynamic/+page.svelte
@@ -14,8 +14,9 @@
 		parents: Node[];
 		level: number;
 		bundle?: Bundle;
-		bundles: Bundle[];
-		bundles_index?: Map<string, Bundle[]>;
+		bundles: Bundles;
+		parents_index?: Map<string, number[]>;
+		bundlesKeys?: string[];
 		links?: Link[];
 		height: number;
 		width?: number;
@@ -41,6 +42,11 @@
 		y: number;
 	}
 
+	interface Bundles {
+		bundles: Bundle[][];
+		i: number;
+	}
+
 	interface Level {
 		nodes: Node[];
 		length: number;
@@ -50,14 +56,14 @@
 		source: Node;
 		bundle: Bundle;
 		target: Node;
-		xt: number;
-		yt: number;
-		yb: number;
-		ys: number;
+		x_target: number;
+		y_target: number;
+		y_bundle: number;
+		y_source: number;
 		c1: number;
 		c2: number;
-		xb: number;
-		xs: number;
+		x_bundle: number;
+		x_source: number;
 	}
 
 	const data = [
@@ -254,12 +260,12 @@
 				const link: Link = {
 					c1: 0,
 					c2: 0,
-					xb: 0,
-					xs: 0,
-					xt: 0,
-					yb: 0,
-					ys: 0,
-					yt: 0,
+					x_bundle: 0,
+					x_source: 0,
+					x_target: 0,
+					y_bundle: 0,
+					y_source: 0,
+					y_target: 0,
 					source: d,
 					bundle: d.bundle,
 					target: p
@@ -271,16 +277,16 @@
 
 		// reverse pointer from parent to bundles
 		bundles.forEach((b: Bundle) => {
-			b.parents.forEach((p: Node) => {
-				if (p.bundles_index === undefined) {
-					p.bundles_index = new Map<string, Bundle[]>();
+			b.parents.forEach((node: Node) => {
+				if (node.parents_index === undefined) {
+					node.parents_index = new Map<string, Bundle[]>();
 				}
-				let value = p.bundles_index.get(b.id);
+				let value = node.parents_index.get(b.id);
 				if (value === undefined) {
 					value = [];
 				}
 				value.push(b);
-				p.bundles_index.set(b.id, value);
+				node.parents_index.set(b.id, value);
 				value.forEach((bundle: Bundle) => {
 					bundlesMap.set(bundle.id, bundle);
 				});
@@ -288,15 +294,18 @@
 		});
 
 		nodes.forEach((node: Node) => {
-			if (node.bundles_index !== undefined) {
-				node.bundles = Array.from(node.bundles_index.values());
+			if (node.parents_index !== undefined) {
+				node.bundles = { bundles: Array.from(node.parents_index.values()), i: 0 };
+				console.log('node.bundles', node.bundles);
 			} else {
-				node.bundles_index = new Map<string, Bundle[]>();
-				node.bundles = [];
+				node.parents_index = new Map<string, number[]>();
+				node.bundles = { bundles: [], i: 0 };
 			}
 
-			node.bundles.sort((a, b) => d3.descending(d3.max(a, d => d.span), d3.max(b, d => d.span)));
-			node.bundles.forEach((b, i) => (b.i = i));
+			node.bundles.bundles.sort((a: Bundle[], b: Bundle[]) => d3.descending(d3.max(a, d => d.span), d3.max(b, d => d.span)));
+			node.bundles.bundles.forEach((b: Bundle[], i) => (b.i = i));
+			console.log('again node.bundles', node.bundles);
+			console.log('again node.bundles_index', node.parents_index);
 		});
 
 		links.forEach((link: Link) => {
@@ -314,7 +323,7 @@
 		options.bigc ||= node_width + c;
 
 		nodes.forEach((node: Node) => {
-				node.height = (Math.max(1, node.bundles.length) - 1) * metro_d;
+				node.height = (Math.max(1, node.bundles.bundles.length) - 1) * metro_d;
 			}
 		);
 
@@ -325,11 +334,11 @@
 			const levelBundles: Bundle[] = bundles.filter(b => b.level === level.nodes[0].level);
 			x_offset += levelBundles.length * bundle_width;
 			y_offset += level_y_padding;
-			level.nodes.forEach((n: Node) => {
-				n.x = n.level * node_width + x_offset;
-				n.y = node_height + y_offset + n.height / 2;
+			level.nodes.forEach((node: Node) => {
+				node.x = node.level * node_width + x_offset;
+				node.y = node_height + y_offset + node.height / 2;
 
-				y_offset += node_height + n.height;
+				y_offset += node_height + node.height;
 			});
 		});
 
@@ -349,24 +358,21 @@
 		});
 
 		links.forEach((link: Link) => {
-			if (link.target.bundles_index === undefined) {
+			if (link.target.parents_index === undefined || !bundlesMap.has(link.bundle.id)) {
 				return;
 			}
-			const linkBundle: Bundle | undefined = bundlesMap.get(link.bundle.id);
-			if (linkBundle === undefined) {
-				return;
-			}
+			const linkBundle: Bundle = bundlesMap.get(link.bundle.id) as Bundle;
 
-			link.xt = link.target.x;
-			link.yt =
+			link.x_target = link.target.x;
+			link.y_target =
 				link.target.y +
 				linkBundle.i * metro_d -
-				(link.target.bundles.length * metro_d) / 2 +
+				(link.target.bundles.bundles.length * metro_d) / 2 +
 				metro_d / 2;
-			link.xb = linkBundle.x;
-			link.yb = linkBundle.y;
-			link.xs = link.source.x;
-			link.ys = link.source.y;
+			link.x_bundle = linkBundle.x;
+			link.y_bundle = linkBundle.y;
+			link.x_source = link.source.x;
+			link.y_source = link.source.y;
 		});
 
 		// compress vertical space
@@ -375,29 +381,28 @@
 		levels.forEach((level: Level) => {
 			const levelBundles: Bundle[] = bundles.filter(b => b.level === level.nodes[0].level);
 			const minimum: number = d3.min(levelBundles, bundle =>
-				d3.min(bundle.links, link => link.ys - 2 * c - (link.yt + c))
+				d3.min(bundle.links, link => link.y_source - 2 * c - (link.y_target + c))
 			) || 0;
 			y_negative_offset +=
 				-min_family_height + minimum;
 			level.nodes.forEach((node: Node) => (node.y -= y_negative_offset));
 		});
 
-		// very ugly, I know
 		links.forEach((link: Link) => {
-			if (link.target.bundles_index === undefined) {
+			if (link.target.parents_index === undefined) {
 				return;
 			}
-			const targetBundle: Bundle[] | undefined = link.target.bundles_index.get(link.bundle.id);
+			const targetBundle: Bundle[] | undefined = link.target.parents_index.get(link.bundle.id);
 			if (targetBundle === undefined) {
 				return;
 			}
-			const c1 = link.source.level - link.target.level > 1 ? Math.min(options.bigc, link.xb - link.xt, link.yb - link.yt) - c : c;
-			link.yt =
+			const c1 = link.source.level - link.target.level > 1 ? Math.min(options.bigc, link.x_bundle - link.x_target, link.y_bundle - link.y_target) - c : c;
+			link.y_target =
 				link.target.y +
 				targetBundle.i * metro_d -
-				(link.target.bundles.length * metro_d) / 2 +
+				(link.target.bundles.bundles.length * metro_d) / 2 +
 				metro_d / 2;
-			link.ys = link.source.y;
+			link.y_source = link.source.y;
 			link.c1 = c1;
 			link.c2 = c;
 		});
@@ -415,12 +420,12 @@
 
 		return { levels, nodes, nodes_index: nodesMap, links, bundles, layout };
 	};
-	let color = d3.scaleOrdinal(d3.schemeDark2);
+	let d3Color = d3.scaleOrdinal(d3.schemeDark2);
+	const color = (d: any, i: number) => d3Color(i);
 	const renderChart = (data: any[]) => {
-		options.color ||= (d: any, i: number) => color(i);
 
-		const nodes: Node[][] = data.map((greekGods: GreekGod[]) => greekGods.map((god: GreekGod) => ({ data: god })));
-		const levels: Level[] = nodes.map((n: Node[]) => ({ nodes: n, bundles: [], length: n.length}));
+		const nodes: Node[][] = data.map((greekGods: GreekGod[]) => greekGods.map((god: GreekGod) => ({ data: god } as Node)));
+		const levels: Level[] = nodes.map((n: Node[]) => ({ nodes: n, bundles: [], length: n.length }));
 
 		return constructTangleLayout(levels);
 	};
@@ -449,22 +454,22 @@
 				{#if !isNaN(l.c1) }
 					<path class="link"
 								d="
-							M{l.xt} {l.yt}
-							L{l.xb - l.c1} {l.yt}
-							A{l.c1} {l.c1} 90 0 1 {l.xb} {l.yt + l.c1}
-							L{l.xb} {l.ys - l.c2}
-							A{l.c2} {l.c2} 90 0 0 {l.xb + l.c2} {l.ys}
-							L{l.xs} {l.ys}"
+							M{l.x_target} {l.y_target}
+							L{l.x_bundle - l.c1} {l.y_target}
+							A{l.c1} {l.c1} 90 0 1 {l.x_bundle} {l.y_target + l.c1}
+							L{l.x_bundle} {l.y_source - l.c2}
+							A{l.c2} {l.c2} 90 0 0 {l.x_bundle + l.c2} {l.y_source}
+							L{l.x_source} {l.y_source}"
 								stroke="{background_color}" stroke-width="5" />
 					<path class="link"
 								d="
-							M{l.xt} {l.yt}
-							L{l.xb - l.c1} {l.yt}
-							A{l.c1} {l.c1} 90 0 1 {l.xb} {l.yt + l.c1}
-							L{l.xb} {l.ys - l.c2}
-							A{l.c2} {l.c2} 90 0 0 {l.xb + l.c2} {l.ys}
-							L{l.xs} {l.ys}"
-								stroke="{options.color(b, i)}" stroke-width="2" />
+							M{l.x_target} {l.y_target}
+							L{l.x_bundle - l.c1} {l.y_target}
+							A{l.c1} {l.c1} 90 0 1 {l.x_bundle} {l.y_target + l.c1}
+							L{l.x_bundle} {l.y_source - l.c2}
+							A{l.c2} {l.c2} 90 0 0 {l.x_bundle + l.c2} {l.y_source}
+							L{l.x_source} {l.y_source}"
+								stroke="{color(b, i)}" stroke-width="2" />
 				{/if}
 			{/each}
 		{/each}
