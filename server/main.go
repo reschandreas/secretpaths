fd6e73fe18c5c55752ae7999123a55e28d9875f1
Andreas Resch <andreas@resch.io>
2024-07-21 21:26:45 +0200

    add dockerfiles

--- server/main.go
+++ server/main.go
@@ -81,46 +81,8 @@ func main() {
 	router.GET("/policies", getPoliciesAPI)
 	router.GET("/analyzedSecrets", getAnalyzedSecretsAPI)
 
-	err := router.Run("localhost:8080")
+	err := router.Run("0.0.0.0:8080")
 	if err != nil {
 		return
-	}
-}
-
-func print_policies() {
-	ctx := context.Background()
-
-	client := backend.UseToken("my-token")
-
-	policies, err := getPolicies(ctx, client)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("rules:", policies)
-	for _, policy := range policies {
-		log.Printf("policy: %s has %d rules", policy.Name, policy.AmountOfPolicies())
-		if policy.AmountOfPolicies() > 0 {
-			for _, rule := range policy.Rules {
-				log.Printf("rule: %s has %s capabilities", rule.Path, rule.Capabilities)
-			}
-		}
-	}
-	log.Println("policies:", len(policies))
-	log.Println("")
-	log.Println("")
-	log.Println("")
-
-	needle := "secret/bar/zip"
-
-	for _, policy := range policies {
-		if policy.Name != "policy_testing" {
-			continue
-		}
-		if policy.HasAccessTo(needle) {
-			log.Printf("policy: %s has access to %s", policy.Name, needle)
-		} else {
-			log.Printf("policy: %s has no access to %s", policy.Name, needle)
-		}
 	}
 }
